01fc60cec1bec66d415aa165835575368a6bc33d
Zhan Rongrui <2742392377@qq.com>
2023-12-09 12:17:02 +0800

    ✅ tests: update test

--- tests/test_classify_record.py
+++ tests/test_classify_record.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
-from auto_ddns.update_dnspod import classify_record
+from auto_ddns.update_dnspod import Client
 
 
-def test_classify_record():
-    assert classify_record("name1.group1") == ("name1.group1", "CNAME")
-    assert classify_record("192.168.1.1") == ("192.168.1.1", "A")
+def test_update_dnspod():
+    client = Client("bone6.com")
+    assert client.classify_record("name1.group1") == ("name1.group1", "CNAME")
+    assert client.classify_record("192.168.1.1") == ("192.168.1.1", "A")
