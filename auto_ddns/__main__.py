84486942136687fedfdbc3a7afbe229f1f431f89
zrr1999 <2742392377@qq.com>
2023-12-25 14:54:28 +0800

    💄 style: improve colors

--- auto_ddns/__main__.py
+++ auto_ddns/__main__.py
@@ -3,18 +3,19 @@ from __future__ import annotations
 import typer
 from loguru import logger
 from rich.console import Console
+from rich.highlighter import NullHighlighter
 from rich.logging import RichHandler
 from rich.style import Style
 
 from auto_ddns import update_dnspod
 
 app = typer.Typer()
+handler = RichHandler(console=Console(style=Style()), highlighter=NullHighlighter(), markup=True)
 
 
 @app.command()
 def update_records_from_json(path: str = typer.Argument("~/.config/autoconfig/auto-ddns.json"), *, log_level="INFO"):
     logger.remove()
-    handler = RichHandler(console=Console(style=Style()), markup=True)
     logger.add(handler, format="{message}", level=log_level)
     logger.info(f"Loading config from [bold purple]{path}[/]")
     update_dnspod.update_records_from_json(path)
--- auto_ddns/set_dns/base.py
+++ auto_ddns/set_dns/base.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import re
+from abc import abstractmethod
 
 from loguru import logger
 
@@ -29,27 +30,34 @@ class DNSSetterBase:
         new_records = self.get_records()
         for subdomain, record in new_records.items():
             if subdomain not in self.records_cache:
-                # self.create_record(record)
+                self.create_record(record)
                 self.records_cache[subdomain] = record
-                logger.info(f"[green](Created ✨)[/] [bold blue]{record.type:<5}[/]: {subdomain} -> {record.value}")
+                logger.info(
+                    f"[green](Created ✨)[/] [bold blue]{record.type:<5}[/]: {subdomain} [bold blue]➡️[/] {record.value}"
+                )
             elif record != new_records[subdomain]:
-                # self.modify_record(self.get_record_id(record), record)
+                self.modify_record(self.get_record_id(record), record)
                 self.records_cache[subdomain] = record
                 logger.info(f"[yellow](Modified 🔄)[/] [bold blue]{record.type:<5}[/]: {subdomain} -> {record.value}")
 
         # TODO: delete records which are not in new_records
 
+    @abstractmethod
     def init_records_cache(self) -> None:
-        raise NotImplementedError("You must implement init_records_cache method")
+        pass
 
+    @abstractmethod
     def get_record_id(self, record: Record) -> str:
-        raise NotImplementedError("You must implement get_record_id method")
+        pass
 
+    @abstractmethod
     def create_record(self, record: Record):
-        raise NotImplementedError("You must implement create_record method")
+        pass
 
+    @abstractmethod
     def delete_record(self, record_id: str):
-        raise NotImplementedError("You must implement delete_record method")
+        pass
 
+    @abstractmethod
     def modify_record(self, record_id: str, record: Record):
-        raise NotImplementedError("You must implement modify_record method")
+        pass
