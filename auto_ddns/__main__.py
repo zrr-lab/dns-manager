bb1d129c907953f39555ae19a20ce64e6bbb576b
zrr1999 <2742392377@qq.com>
2024-01-16 14:21:44 +0800

    ✨ feat: support watch config in  daemon command

--- auto_ddns/__main__.py
+++ auto_ddns/__main__.py
@@ -1,16 +1,18 @@
 from __future__ import annotations
 
+import asyncio
 import json
-import time
 from pathlib import Path
 
 import typer
+import watchfiles
 from loguru import logger
 from rich.console import Console
 from rich.highlighter import NullHighlighter
 from rich.logging import RichHandler
 from rich.style import Style
 
+from auto_ddns.setter import DNSSetterBase
 from auto_ddns.utils import create_setter_by_str
 
 app = typer.Typer()
@@ -35,15 +37,49 @@ def update(path: Path = Path("~/.config/autoconfig/auto-ddns.json"), setter: str
 @app.command()
 def daemon(path: Path = Path("~/.config/autoconfig/auto-ddns.json"), setter: str = "dnspod", log_level: str = "INFO"):
     init_logger(log_level)
-    logger.info(f"Loading config from [bold purple]{path}[/]")
-    with open(path.expanduser()) as f:
-        config = json.load(f)
-    setter_obj = create_setter_by_str(config, setter)
-    while True:
-        setter_obj.update_dns()
-        interval = 300
-        logger.info(f"Sleeping for {interval} seconds")
-        time.sleep(interval)
+    path = path.expanduser()
+    queue = asyncio.Queue()
+
+    async def watch_config(queue: asyncio.Queue[DNSSetterBase]):
+        logger.info(f"Loading config from [bold purple]{path}[/]")
+        with open(path) as f:
+            config = json.load(f)
+        setter_obj = create_setter_by_str(config, setter)
+        await queue.put(setter_obj)
+
+        for _ in watchfiles.watch(path):
+            logger.info(f"Config file changed, reloading config from [bold purple]{path}[/]")
+            with open(path) as f:
+                config = json.load(f)
+            setter_obj = create_setter_by_str(config, setter)
+            await queue.put(setter_obj)
+
+    async def update_dns(queue: asyncio.Queue[DNSSetterBase]):
+        setter_obj = await queue.get()
+        while True:
+            setter_obj.update_dns()
+            interval = 300
+            logger.info(f"Sleeping for {interval} seconds")
+            while True:
+                await asyncio.sleep(1)
+                if not queue.empty():
+                    setter_obj = await queue.get()
+                    break
+
+    watch_task = asyncio.create_task(watch_config(queue))
+    update_task = asyncio.create_task(update_dns(queue))
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(
+            asyncio.gather(
+                watch_task,
+                update_task,
+            )
+        )
+    finally:
+        loop.close()
 
 
 if __name__ == "__main__":
