23386a50876147aeaba896bf2c132493cfebf71f
zrr1999 <2742392377@qq.com>
2024-02-23 13:11:15 +0800

    ♻️ refactor: change deault config path

--- dns_manager/__main__.py
+++ dns_manager/__main__.py
@@ -25,7 +25,7 @@ def init_logger(log_level: str):
 
 
 @app.command()
-def update(path: Path = Path("~/.config/autoconfig/dns-manager.json"), setter: str = "dnspod", log_level: str = "INFO"):
+def update(path: Path = Path("~/.config/dns-manager/ddns.json"), setter: str = "dnspod", log_level: str = "INFO"):
     init_logger(log_level)
     logger.info(f"Loading dns config from [bold purple]{path}[/].")
     config = load_config(path)
@@ -34,7 +34,7 @@ def update(path: Path = Path("~/.config/autoconfig/dns-manager.json"), setter: s
 
 
 @app.command()
-def watch(path: Path = Path("~/.config/autoconfig/dns-manager.json"), setter: str = "dnspod", log_level: str = "INFO"):
+def watch(path: Path = Path("~/.config/dns-manager/ddns.json"), setter: str = "dnspod", log_level: str = "INFO"):
     init_logger(log_level)
     path = path.expanduser()
     asyncio.run(watch_async(path, setter))
